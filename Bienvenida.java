40ea9fe5577bd2d657ec59ec95b7a3bf77449fb7
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-13 14:37:11 -0300

    Agrego logo al titulo

--- Bienvenida.java
+++ Bienvenida.java
@@ -14,5 +14,10 @@ public class Bienvenida extends JFrame implements ActionListener{
     setTitle("¡Bienvenido!");
     getContentPane().setBackground(new Color(255,0,0));
     setIconImage(new ImageIcon(getClass().getResource("images/icon.png")).getImage());
+
+    ImageIcon imagen = new ImageIcon("images/logo-coca.png");
+    label1 = new JLabel(imagen);
+    label1.setBounds(25,15,300,150);
+    add(label1);
   }
 }
