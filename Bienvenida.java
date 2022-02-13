81db160e257b8c77cacf42299cd244357b86ef96
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-13 20:33:13 -0300

    Creando labels

--- Bienvenida.java
+++ Bienvenida.java
@@ -19,5 +19,23 @@ public class Bienvenida extends JFrame implements ActionListener{
     label1 = new JLabel(imagen);
     label1.setBounds(25,15,300,150);
     add(label1);
+
+    label2 = new JLabel("Sistema de Control Vacacional");
+    label2.setBounds(35,135,300,30);
+    label2.setFont(new Font("Andale Mono", 3, 18));
+    label2.setForeground(new Color(255,255,255));
+    add(label2);
+
+    label3 = new JLabel("Ingrese su nombre");
+    label3.setBounds(45,212,200,30);
+    label3.setFont(new Font("Andale Mono", 1, 12));
+    label3.setForeground(new Color(255,255,255));
+    add(label3);
+
+    label4 = new JLabel("©2022 The Coca-Cola Company");
+    label4.setBounds(85,375,300,30);
+    label4.setFont(new Font("Andale Mono", 3, 12));
+    label4.setForeground(new Color(255,255,255));
+    add(label4);
   }
 }
